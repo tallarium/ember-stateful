836d928f7763083e8891954350fb61e838a18e33
Michal Szewczak <michal@szewczak.email>
2017-11-10 19:09:09 +0000

    export waitForState from index

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,8 @@
 import StatefulMixin from 'ember-stateful/mixins/stateful';
 import waitForEnterState from 'ember-stateful/utils/wait-for-enter-state';
+import waitForState from 'ember-stateful/utils/wait-for-enter-state'
 import ERRORS from 'ember-stateful/errors';
 
-export { ERRORS, waitForEnterState };
+export { ERRORS, waitForEnterState, waitForState };
 export default StatefulMixin;
 
